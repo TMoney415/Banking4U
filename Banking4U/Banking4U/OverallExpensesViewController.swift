365a19fb2fb6ea49071d0ee38c348099dac14a64
Anthony Delucchi <adelucchi2017@room208-11.local>
2016-05-16 12:06:28 -0700

    code update to overall expense page

---
+++ Banking4U/Banking4U/OverallExpensesViewController.swift
@@ -0,0 +1,26 @@
+//
+//  OverallExpensesViewController.swift
+//  Banking4U
+//
+//  Created by Anthony Delucchi on 5/16/16.
+//  Copyright © 2016 Tyler Ray. All rights reserved.
+//
+
+import UIKit
+
+class OverallExpensesViewController: UIViewController {
+    
+    var type = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
