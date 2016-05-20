0ff17644dda6a5183bdefe043ae31fbaaa1b76ff
Anthony Delucchi <adelucchi2017@room208-11.local>
2016-05-20 12:53:38 -0700

    big code update

--- Banking4U/Banking4U/OverallExpensesViewController.swift
+++ Banking4U/Banking4U/OverallExpensesViewController.swift
@@ -11,16 +11,59 @@ import UIKit
 class OverallExpensesViewController: UIViewController {
     
     var type = ""
+    var propertyValueNumber:Double?
+    var propertyTax:String?
+    var foodExpenseInputNumber:Double?
+    var clothingExpenseInputNumber:Double?
+    var travelExpenseInputNumber:Double?
+    var overallExpensesNumber:Double?
+    var foodConfirm:Bool?
+    var clothingConfirm:Bool?
+    var travelConfirm:Bool?
 
+    @IBOutlet weak var foodExpenseBox: UITextField!
+    @IBOutlet weak var clothingExpenseBox: UITextField!
+    @IBOutlet weak var travelExpenseBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print(type)
+        print(propertyTax)
+        print(propertyValueNumber)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    @IBAction func foodExpenseInput(sender: AnyObject) {
+        if let inputStr = foodExpenseBox.text, foodExpense = Double(inputStr) {
+            foodExpenseInputNumber = foodExpense
+            foodConfirm = true
+        }
+        overallExpenses()
+    }
+    @IBAction func clothingExpenseInput(sender: AnyObject) {
+        if let inputStr = clothingExpenseBox.text, clothingExpense = Double(inputStr) {
+            clothingExpenseInputNumber = clothingExpense
+            clothingConfirm = true
+        }
+        overallExpenses()
+    }
+    @IBAction func travelExpenseInput(sender: AnyObject) {
+        if let inputStr = travelExpenseBox.text, travelExpense = Double(inputStr) {
+            travelExpenseInputNumber = travelExpense
+            travelConfirm = true
+        }
+        overallExpenses()
+    }
+//------------------------------------------------------------------------------------
+    func overallExpenses() {
+        if foodConfirm == true && clothingConfirm == true && travelConfirm == true {
+                overallExpensesNumber = travelExpenseInputNumber! + clothingExpenseInputNumber! + foodExpenseInputNumber! + propertyValueNumber!
+        }
+        print(overallExpensesNumber)
+//fix this code-----------------------------------------------------------------------
+    }
 }
